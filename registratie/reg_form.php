5947666802766bcd211433b96c0c23cf7e140b4d
Alexander <alexander.12@live.nl>
2018-09-26 13:25:45 +0200

    email naampje aangemaakt
    ik heb een naapje met 'email' aangemaakt, er volgt hierna nog eentje

--- registratie/reg_form.php
+++ registratie/reg_form.php
@@ -13,6 +13,7 @@
 <footer>
     <div id="font1">username</div> <input class="kleur-input" type="text" name="username"/><br>
     <div id="font1">password</div> <input class="kleur-input" type="password" name="password"/><br>
+    <div id="font1">email</div> <input class="kleur-input" type="text" name="username"/><br>
 </footer>
 
 
