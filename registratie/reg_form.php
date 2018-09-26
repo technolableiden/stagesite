e5396c621f565f89f329eb4521a1addf03636996
Alexander <alexander.12@live.nl>
2018-09-26 13:18:43 +0200

    form aangemaakt
    ik heb de registratie formulier aangemaakt (beginsel)

--- registratie/reg_form.php
+++ registratie/reg_form.php
@@ -6,7 +6,9 @@
     <img id="logo" alt="Logo Technolab" src="img/WhatsApp%20Image%202018-09-20%20at%2010.44.00.jpeg">
 </header>
 <body>
+<form id="form-reg" method="get" action="reg.php">
 
+</form>
 </body>
 <footer>
 
