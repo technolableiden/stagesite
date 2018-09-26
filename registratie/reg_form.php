7a4a7976da8f73412e68c27e1a8e727a9eff0216
Alexander <alexander.12@live.nl>
2018-09-26 13:07:15 +0200

    afbeelding ingevoegd in php
    ik heb de onlangs toegevoegde afbeelding toegevoegd aan de php file

--- registratie/reg_form.php
+++ registratie/reg_form.php
@@ -3,7 +3,7 @@
     <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <header>
-
+    <img id="logo" alt="Logo Technolab" src="img/WhatsApp%20Image%202018-09-20%20at%2010.44.00.jpeg">
 </header>
 <body>
 
