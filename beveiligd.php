604beb7034c09ea202c949ab1058c7ae04a935cc
natasvbaal <40168548+natasvbaal@users.noreply.github.com>
2018-09-26 13:08:48 +0200

    copyright homepage
    Homepage heeft een copyright kopje

--- beveiligd.php
+++ beveiligd.php
@@ -11,3 +11,27 @@ else{
 }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    <script src="JS/js.js"></script>
+    <meta charset="UTF-8">
+    <meta name="description" content="Stagair local spot">
+    <meta name="keywords" content="Technolab, Stagairs, Workshops">
+    <meta name="author" content="Ravi Breugom, Alexander Wallaard, Natascha van Baal">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="60">
+    <title>Technolab Stagairspot</title>
+</head>
+<header>
+
+</header>
+<body>
+
+</body>
+
+<footer id="copyright">&copy; Technolab Leiden</footer>
+</body>
+</html>
