f7fd22150db71825402c06c2009877a3959329e1
ravinator <36293225+ravinator@users.noreply.github.com>
2018-10-02 15:17:50 +0200

    .

--- Workshops/Workshop.php
+++ Workshops/Workshop.php
@@ -43,7 +43,7 @@
 
                             }
                             else{
-                                header("Location: ../login_form.html");
+                                header("Location: ../login_form.php");
                             }
 
                             ?></a>
