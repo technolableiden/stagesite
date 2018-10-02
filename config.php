d8a97cd638cfc485969dc3dda94a03e97ecb0821
Alexander <alexander.12@live.nl>
2018-10-02 14:35:05 +0200

    config file ingesteld
    ik heb de database connectie z'n eigen file gegeven

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "185.114.157.172";
-$username = "spelsalo";
-$password = "klaas111";
-$dbname = "spelsalo_2";
+$dbHost = "185.114.157.172";        //Location Of Database usually its localhost
+$dbUser = "spelsalo";            //Database User Name
+$dbPass = "klaas111";            //Database Password
+$dbname = "spelsalo_2";    //Database Name
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$con = new mysqli($dbHost, $dbUser, $dbPass, $dbname);
 // Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
 }
 ?>
--- login_form.php
+++ login_form.php
@@ -2,17 +2,7 @@
 
 <?php
 if(isset($_POST['submit'])){
-    $dbHost = "185.114.157.172";        //Location Of Database usually its localhost
-    $dbUser = "spelsalo";            //Database User Name
-    $dbPass = "klaas111";            //Database Password
-    $dbname = "spelsalo_2";    //Database Name
-
-// Create connection
-    $con = new mysqli($dbHost, $dbUser, $dbPass, $dbname);
-// Check connection
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    }
+include('config.php');
 
 
     $uname = mysqli_real_escape_string($con,$_POST['username']);
