21277f73851ff31eed1b3f6c0177a81dccdb5987
natasvbaal <40168548+natasvbaal@users.noreply.github.com>
2018-10-09 10:15:19 +0200

    icons
    icons in div gezet

--- Contact/Contact.php
+++ Contact/Contact.php
@@ -62,11 +62,11 @@
     </div>
 
 </header>
-
-<i class="fa fa-phone" style="font-size:24px"></i>
-<i class="fa fa-envelope-o" style="font-size:24px"></i>
-<i class="fa fa-map-marker" style="font-size:24px"></i>
-
+<div class="icons">
+<i class="fa fa-phone" style="font-size:24px"></i> <br>
+<i class="fa fa-envelope-o" style="font-size:24px"></i> <br>
+<i class="fa fa-map-marker" style="font-size:24px"></i> <br>
+</div>
 
 
 <footer id="copyright">&copy; Technolab Leiden</footer>
