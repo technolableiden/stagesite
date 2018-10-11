f5ee263ff9ac3d7668c83de612425cea638e7fb4
Alexander <alexander.12@live.nl>
2018-10-11 11:13:53 +0200

    spel is nu werkend
    spel is nu volledig werkend

--- Workshops/stroom-geeft-energie/Stroom.php
+++ Workshops/stroom-geeft-energie/Stroom.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="js/js.js"></script>
     <meta charset="UTF-8">
     <meta name="description" content="Stagair local spot">
     <meta name="keywords" content="Technolab, Stagairs, Workshops">
@@ -16,7 +15,9 @@
 <header>
     <div class="custom=padding">
         <nav>
-            <a href="../../beveiligd.php"><img class="logo" src="../../img/WhatsApp%20Image%202018-09-20%20at%2010.44.00.jpeg" alt="Logo"></a>
+            <a href="../../beveiligd.php"><img class="logo"
+                                               src="../../img/WhatsApp%20Image%202018-09-20%20at%2010.44.00.jpeg"
+                                               alt="Logo"></a>
             <ul class="menu-area">
                 <li><a href="#">Workshops</a></li>
                 <li><a href="#">Agenda</a></li>
@@ -31,11 +32,10 @@
                         <a><?php
 
                             session_start();
-                            if($_SESSION['ingelogd'] == "ja"){
-                                echo $_SESSION['username']."<br>";
+                            if ($_SESSION['ingelogd'] == "ja") {
+                                echo $_SESSION['username'] . "<br>";
 
-                            }
-                            else{
+                            } else {
                                 header("Location: ../../login_form.php");
                             }
 
@@ -49,118 +49,33 @@
 </header>
 <div id="border-buiten">
     <h1 id="border-buiten-tekst1">Stroom Geeft energy game.</h1>
-<p id="border-buiten-tekst2">het doel van dit spel is om het lampje van stroom te vorzien! lukt het jouw om het lampje te laten branden?</p>
- <section id="flex-container-pijlen">
-     <img id="pijl-links" alt="pijl-links" src="img/pijl-links.png">
-     <img id="pijl-rechts" alt="pijl-rechts" src="img/pijl-rechts.png">
- </section>
+    <p id="border-buiten-tekst2">het doel van dit spel is om het lampje van stroom te voorzien! lukt het jou om het
+        lampje te laten branden?</p>
+    <section id="flex-container-pijlen">
+        <img id="pijl-links" alt="pijl-links" src="img/pijl-links.png">
+        <img id="pijl-rechts" alt="pijl-rechts" src="img/pijl-rechts.png">
+    </section>
 
     <canvas id="border-spel">
-
 
 
     </canvas>
 
     <!--spel plaatjes-->
     <div id="gloeilamp">
-        <img src="img/gloeilamp-uit.png"/>
-        <img src="img/gloeilamp-aan.jpg" style="display:none;"/>
+        <img id="lamp-uit" src="img/gloeilamp-uit.png"/>
+        <img id="lamp-aan" src="img/gloeilamp-aan.png"/>
     </div>
-<img id="target" src="img/schakkelaar.png"/>
+    <img id="target" src="img/schakkelaar.png"/>
     <script src="./bl.ocks.org_files/rotate.js.download"></script>
     <img id="target2" src="img/schakkelaar.png"/>
     <script src="./bl.ocks.org_files/rotate2.js.download"></script>
-<button id="zetaan-knop" onclick="activate()" >zet aan</button>
+    <button id="zetaan-knop" onclick="activate()">zet aan</button>
+    <script src="js/js.js"></script>
 
 
+    <footer id="copyright">&copy; Technolab Leiden</footer>
 
 
-
-
-
-
-
-<footer id="copyright">&copy; Technolab Leiden</footer>
-    <script>
-
-
-</script>
-
-
-
-
-
-<!--
-    <script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       var el2 = document.getElementById("target2");
-       var st2 = window.getComputedStyle(el2, null);
-       var tr2 = st2.getPropertyValue("-webkit-transform") ||
-           st2.getPropertyValue("-moz-transform") ||
-           st2.getPropertyValue("-ms-transform") ||
-           st2.getPropertyValue("-o-transform") ||
-           st2.getPropertyValue("transform") ||
-           "FAIL";
-
-       // With rotate(30deg)...
-       // matrix(0.866025, 0.5, -0.5, 0.866025, 0px, 0px)
-       //console.log('Matrix: ' + tr);
-
-       // rotation matrix - http://en.wikipedia.org/wiki/Rotation_matrix
-
-       var values2 = tr2.split('(')[1].split(')')[0].split(',');
-       var a2 = values[0];
-       var b2 = values[1];
-       var c2 = values[2];
-       var d2 = values[3];
-
-       var scale2 = Math.sqrt(a2*a2 + b2*b2);
-
-       //console.log('Scale: ' + scale);
-
-       // arc sin, convert from radians to degrees, round
-       var sin2 = b2/scale2;
-       // next line works for 30deg but not 130deg (returns 50);
-        var angle2 = Math.round(Math.asin(sin2) * (180/Math.PI));
-      // var angle2 = Math.round(Math.atan2(b2, a2) * (180/Math.PI));
-
-       console.log('Rotate: ' + angle2 + 'deg');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </script>
-    -->
 </body>
 </html>
--- Workshops/stroom-geeft-energie/js/js.js
+++ Workshops/stroom-geeft-energie/js/js.js
@@ -1,33 +1,8 @@
 function activate() {
 
 
-    // var el = document.getElementById("target");
 
-    // var st = window.getComputedStyle(el, null);
-
-    //var tr = st.getPropertyValue("-webkit-transform") ||
-    //  st.getPropertyValue("-moz-transform") ||
-    // st.getPropertyValue("-ms-transform") ||
-    //st.getPropertyValue("-o-transform") ||
-    // st.getPropertyValue("transform") ||
-    //"Either no transform set, or browser doesn't do getComputedStyle";
-    // alert(tr);
-
-    //rotate(xDeg) = matrix(cos(x), sin(x), -sin(x), cos(x), 0, 0);
-
-    // var values = tr.split('(')[1],
-    //    values = values.split('(')[0],
-    //   values = values.split(',');
-
-    //   var a = values[0];
-    //  var b = values[1];
-    //   var c = values[2];
-    // var d = values[3];
-
-    //  var angle = math.round(Math.asin(b) * (180/Math.PI));
-    // console.log(angle);
-
-
+//rotating energy cable1
     var el = document.getElementById("target");
     var st = window.getComputedStyle(el, null);
     var tr = st.getPropertyValue("-webkit-transform") ||
@@ -37,12 +12,6 @@ function activate() {
         st.getPropertyValue("transform") ||
         "FAIL";
 
-    // With rotate(30deg)...
-    // matrix(0.866025, 0.5, -0.5, 0.866025, 0px, 0px)
-    //console.log('Matrix: ' + tr);
-
-    // rotation matrix - http://en.wikipedia.org/wiki/Rotation_matrix
-
     var values = tr.split('(')[1].split(')')[0].split(',');
     var a = values[0];
     var b = values[1];
@@ -51,20 +20,80 @@ function activate() {
 
     var scale = Math.sqrt(a*a + b*b);
 
-    //console.log('Scale: ' + scale);
-
-    // arc sin, convert from radians to degrees, round
     var sin = b/scale;
-    // next line works for 30deg but not 130deg (returns 50);
+
     var angle = Math.round(Math.asin(sin) * (180/Math.PI));
-    //var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
 
     console.log('Rotate: ' + angle + 'deg');
 
-    //if (angle == -89) {
-        //alert('nice');
-    //}
-    if (angle == -89) {
-        alert('its shining!');
-    }
+
+
+
+
+
+
+    //rotating energy cable 2
+
+    var el2 = document.getElementById("target2");
+    var st2 = window.getComputedStyle(el2, null);
+    var tr2 = st2.getPropertyValue("-webkit-transform") ||
+        st2.getPropertyValue("-moz-transform") ||
+        st2.getPropertyValue("-ms-transform") ||
+        st2.getPropertyValue("-o-transform") ||
+        st2.getPropertyValue("transform") ||
+        "FAIL";
+
+
+
+
+    var values2 = tr2.split('(')[1].split(')')[0].split(',');
+    var a2 = values2[0];
+    var b2 = values2[1];
+    var c2 = values2[2];
+    var d2 = values2[3];
+
+    var scale2 = Math.sqrt(a2*a2 + b2*b2);
+
+
+    var sin2 = b2/scale2;
+    var angle2 = Math.round(Math.asin(sin2) * (180/Math.PI));
+    console.log('Rotate: ' + angle2 + 'deg');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//seeing if the power cables are well rotated, if it's true then do something
+
+     if ((angle > -9 && angle < 10) && (angle2 > -9 && angle2 < 10)){
+       document.getElementById('lamp-aan').style.visibility = "visible";
+        document.getElementById('lamp-uit').style.visibility = "hidden";
+          // alert('its shining!');
+       }
+       else{
+           document.getElementById('lamp-aan').style.visibility = "hidden";
+           document.getElementById('lamp-uit').style.visibility = "visible";
+           // alert('you suck');
+        }
 }
+
+
+
+
+
+
+
+
