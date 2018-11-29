bf6758bdd6174b34fcb0dcf3320b0a7a8d486ee9
Alexander <alexander.12@live.nl>
2018-11-29 10:32:30 +0100

    excel omgezet naar outlook
    ik heb de excel sheet imbedding omgezet naar eentje van outlook die vast zit aan de agenda van teams

--- agenda/agenda.php
+++ agenda/agenda.php
@@ -76,7 +76,7 @@
 </header>
 <div id="grote-blok">
     <h1 id="h-agenda">Agenda</h1>
-    <iframe id="excel" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbCxqVS2tcQLOPnyLmroa3oMdWvUPEMjB_I5wOXSN6bujL_fjIiiRCJAl3ZbBbgSj7vdNu-ywoO5AN/pubhtml?gid=1348854223&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+    <iframe id="excel" src="https://outlook.office365.com/owa/calendar/587a4593c5e14cbeb35550d51c3d20dc@technolableiden.nl/3dec10380d254562a3e727cfe9b94ba111424249706447017719/calendar.html"></iframe>
 <!--link om sheet aan te passen: https://docs.google.com/spreadsheets/d/1YjCc5LHqA4otVWVo9Co5mBFEbtXvbFvpji_fC7hoQGI/edit#gid=1348854223 -->
 <p id="blok-info">Op deze pagina kan je zien waar en wanneer je ergens moet zijn! aan de rechterzijde heb je ook een vak om in te klokken, doe dit zodra je binnen komt om gedoe te vorkomen! thuis inklokken heeft geen zin omdat het IP strict is</p>
 </div>
